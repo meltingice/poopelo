6516492d90f1408efa4e03d6ae85e8e610a8210b
Ryan LeFevre <meltingice8917@gmail.com>
2017-06-07 14:11:27 -0400

    Remove service worker crap

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { unregister } from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
+
