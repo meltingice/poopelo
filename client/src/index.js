962577caca1ff3d2e41b95f942c39526376e2b8c
Ryan LeFevre <meltingice8917@gmail.com>
2017-06-07 14:03:43 -0400

    Update build

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { unregister } from './registerServiceWorker';
+import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-unregister();
+registerServiceWorker();
